2a25fcf58df5ce73c906a0835b843b9408c28619
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-08-08 11:41:41 +0700

    fix: NaN on printf; new: type for virtual pointer;

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
+type ValuesConv = {
+  value: any,
+  isValue: boolean
+}
 export function printf(str: string, ...value: any[]) {
   let arr_str = str.split("");
   for (let i = 0; i < str.length - 1; i++) {
-    let value_conv;
-    if (arr_str[i] === "%") {
+    let value_conv: ValuesConv = {
+      value: "",
+      isValue: false
+    };
+    if (arr_str[i] === "%" && !!value[0]) {
       switch (arr_str[i + 1]) {
         case "i":
         case "d":
-          value_conv = Math.floor(value[0]);
+          value_conv.value = Math.floor(value[0]);
+          value_conv.isValue = true;
           break;
         case "f":
-          value_conv = parseFloat(value[0]);
+          value_conv.value = parseFloat(value[0]);
+          value_conv.isValue = true;
           break;
         case "s":
         case "c":
-          value_conv = value[0].toString();
+          value_conv.value = value[0].toString();
+          value_conv.isValue = true;
           break;
       }
-      if (value_conv) {
-        arr_str[i] = value_conv;
+      if (value_conv.isValue) {
+        arr_str[i] = value_conv.value;
         value.shift();
         arr_str[i + 1] = "";
       }
@@ -27,12 +37,25 @@ export function printf(str: string, ...value: any[]) {
 }
 
 export type i8 = number;
+export type _i8 = [number];
+
 export type i32 = number;
+export type _i32 = [number];
+
 export type i16 = number;
+export type _i16 = [number];
+
 export type int = number;
+export type _int = [number];
+
 export type float = number;
+export type _float = [number];
+
 export type double = number;
+export type _double = [number];
+
 export type char = string;
+export type _char = [string];
 
 import ifes from "./ife";
 export const ife = ifes;
